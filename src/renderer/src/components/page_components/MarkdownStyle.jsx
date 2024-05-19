27a39fbcc40ac6628b13d7e8d50cf7b56482df92
RajeshTechForge <rajeshmondal04@outlook.com>
2024-05-19 12:48:18 +0530

    Improved Markdown for code-blocks & inline-code #4

--- src/renderer/src/components/page_components/MarkdownStyle.jsx
+++ src/renderer/src/components/page_components/MarkdownStyle.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-import style from './style/MarkdownStyle.module.css'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/github-dark.css'
+import './style/MarkdownStyle.css'
 
 export const MarkdownStyle = (props) => {
+  useEffect(() => {
+    // hljs.configure({ classPrefix: 'dark-hljs-' })
+    hljs.highlightAll()
+  }, [])
+
+  // Remove unwanted newlines from the Markdown content
+  // const formattedText = props.text.replace(/\n\s*\n/g, '\n')
+
   return (
-    <p className={style.text}>
+    <p className="markdownText">
       <Markdown remarkPlugins={[remarkGfm]}>{props.text}</Markdown>
     </p>
   )
