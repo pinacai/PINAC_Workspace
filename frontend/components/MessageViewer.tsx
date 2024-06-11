a5966f5f0b41b0da1abd72db8f2dbb7ef831d1c0
DevanshArora312 <devansharora312@gmail.com>
2024-06-11 16:24:49 +0530

    added loader component and corresponding CSS

--- frontend/components/MessageViewer.tsx
+++ frontend/components/MessageViewer.tsx
@@ -9,7 +9,7 @@ import userIcon from "../assets/icon/user_icon.png";
 import pinacLogo from "../assets/icon/pinac-logo.png";
 
 export const ShowAiMessage: React.FC = () => {
-  const [message, setMessage] = useState(<></>);
+  const [message, setMessage] = useState(<AiLoader/>);
 
   window.ipcRenderer.once("server-response", (_, response) => {
     if (response["response"]["type"] === "email") {
@@ -96,6 +96,41 @@ export const AiMessage: React.FC<AiMessageProps> = (props) => {
           <div className="msg-name">PINAC</div>
           <div className="msg-text ai-msg">
             <MarkdownStyle text={props.response} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+// Creating a AiLoader component similar to AiMessage. message state is initialised with this loader and replaced as soon as we have the data. 
+export const AiLoader: React.FC = () => {
+  const [isAvatarVisible, setIsAvatarVisible] = useState(
+    window.innerWidth > 576
+  ); // Initial state based on window size
+
+  // Handle window resize and update avatar visibility
+  useEffect(() => {
+    const updateAvatarVisibility = () => {
+      setIsAvatarVisible(window.innerWidth > 576);
+    };
+    window.addEventListener("resize", updateAvatarVisibility);
+    // Cleanup function to remove the event listener
+    return () => window.removeEventListener("resize", updateAvatarVisibility);
+  }, []);
+
+  return (
+    <>
+      <div className="msg-row">
+        {isAvatarVisible && (
+          <div className="msg-avatar">
+            <img src={pinacLogo} alt="AI Avatar" />
+          </div>
+        )}
+        <div className="msg-content">
+          <div className="msg-name">PINAC</div>
+          <div className="msg-text ai-msg">
+            <div className="loader"/>
           </div>
         </div>
       </div>
