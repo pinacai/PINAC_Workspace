b4ff889d6e883e455a7d832d7bf53daee026288e
RajeshTechForge <rmondaljamalpur@gmail.com>
2025-04-02 20:07:09 +0530

    Fix: Updated import of UserMsgBubble
    Updates the component used to render markdown in the user message bubble.

--- src/features/msgBubble/UserMsgBubble.tsx
+++ src/features/msgBubble/UserMsgBubble.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MarkdownStyle } from "../../components/MarkdownStyle";
+import { MarkdownRenderer } from "../../components/MarkdownRenderer";
 
 interface UserMsgBubbleProps {
   response: string;
@@ -9,7 +9,7 @@ export const UserMsgBubble: React.FC<UserMsgBubbleProps> = ({ response }) => {
   return (
     <div className="flex justify-end">
       <div className="lg:max-w-[90%] px-4 py-2 rounded-2xl bg-gray-300 dark:bg-tertiary-dark text-lg text-gray-900 dark:text-white">
-        <MarkdownStyle text={response} />
+        <MarkdownRenderer text={response} />
       </div>
     </div>
   );
