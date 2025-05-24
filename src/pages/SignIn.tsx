09e82bf4959d98c634df26d2b1fed96b26a819c2
RajeshTechForge <rmondaljamalpur@gmail.com>
2025-05-24 23:22:49 +0530

    Updating external authentication link

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -10,14 +10,14 @@ const SignInPage: React.FC<SignInPageProps> = ({ isLoading }) => {
   const handleButtonClick = () => {
     window.ipcRenderer.send(
       "open-external-link",
-      "https://pinacworkspace.pages.dev/auth/sign-up/?app-auth=true"
+      "https://pinacworkspace.vercel.app/auth/sign-up/?app-auth=true"
     );
   };
 
   const handleLinkClick = () => {
     window.ipcRenderer.send(
       "open-external-link",
-      "https://pinacworkspace.pages.dev/auth/sign-in/?app-auth=true"
+      "https://pinacworkspace.vercel.app/auth/sign-in/?app-auth=true"
     );
   };
 
